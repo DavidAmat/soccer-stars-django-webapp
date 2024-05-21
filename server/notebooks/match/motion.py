f38a8b262fafa222edcea0b63f6fc3784d36b264
DavidAmat <daolondrizdaolondriz@gmail.com>
2024-05-22 00:21:53 +0200

    feat: first approach friction function

--- server/notebooks/match/motion.py
+++ server/notebooks/match/motion.py
@@ -2,14 +2,14 @@ import numpy as np
 from collision_resolver import CollisionResolver
 
 class Motion:
-    def __init__(self, X, R, M, min_velocity=0.1, boundaries=(1920, 1080)):
+    def __init__(self, R, M, min_velocity=0.1, boundaries=(1920, 1080)):
         self.boundaries = boundaries  # boundaries should be a tuple (width, height)
         self.w, self.h = boundaries
-        self.X = X
         self.R = R
         self.M = M
-        self.n = X.shape[0]  # Number of caps
+        self.n = len(M)
         self.min_velocity = min_velocity
+        self.cr = CollisionResolver(R=R, M=M, boundaries=boundaries)
         
     def get_cap_velocity_modulus(self, V):
         """
@@ -25,33 +25,38 @@ class Motion:
         caps_mod_velocity = self.get_cap_velocity_modulus(V)
         return np.all(caps_mod_velocity < self.min_velocity)
     
-    def main(self, V):
+    def run_motion(self, X, V):
+        positions = [X]
+        velocities = [V]
+        t = 0
+        cr = self.cr
+
         # Check if system is moving
-        self.V = V
         system_moving = self.is_system_moving(V)
         if not system_moving:
-            return self.X, self.V
-         
-        # Duplicate V and X matrix for iterate over them
-        V = self.V.copy()
-        X = self.X.copy()
-            
+            return positions, velocities
+
+        # Use copies of inputs to iterate positions and velocities over timesteps
+        X_t = X.copy()
+        V_t = V.copy()
+        
         # Get the system evolve through the motion dynamics till the system stops moving
         while system_moving:
+            # Update timestep
+            t += 1
 
-            # Calculate the next position X(t+1) = X(t) + V(t)
-            X_next = X + V
+            # Resolve collisions: get V(t+1) and X(t+1)
+            V_t_1 = cr.resolve_collision(X=X_t, V=V_t)
+
+            # Apply friction to the system
+            
+
+            X_t_1 = X_t + V_t_1
 
             # Check if collision in the X_next occurs
+            positions.append(X_t_1)
+            velocities.append(V_t_1)
 
-            # Update cap position after handling potential collisions
-            X = X_next
-            print("--------------------")
-            print("Current Velocity:", V)
-            # Calculate new velocity
-            V = self.next_velocity(V)
-            print("New Velocity:", V)
-            print("--------------------")
         return X, V
     
 if __name__ == "__main__":
